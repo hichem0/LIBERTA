198f9c6974e7304ad5c237f3ffda9bb06a843e08
Redha <benzinered@yahoo.fr>
2022-06-06 14:08:34 +0200

    init back project

---
+++ back/src/main/java/com/eweb/liberta/LibertaApplication.java
@@ -0,0 +1,13 @@
+package com.eweb.liberta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibertaApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/eweb/liberta/LibertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eweb.liberta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class LibertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
